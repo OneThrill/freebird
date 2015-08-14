e0959b434f57b404f2ecbc2e2271ff585a82f0ca
Mic Bersani <Michele.Bersani@learndirect.com>
2015-08-14 11:52:25 +0100

    changed text

--- src/main/java/miky/App.java
+++ src/main/java/miky/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "\nThis is a test application that shows how to use Jenkins, Git and Docker" );
-        System.out.println( "to build, deploy and run a Java application in isolated containers" );
+        System.out.println( "to build, deploy and run a Java application in isolated containers\n" );
     }
 }
