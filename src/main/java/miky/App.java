8c6fe94df6a13dc52cfc3dc9845ca8c2bbfc7b5e
Mic Bersani <Michele.Bersani@learndirect.com>
2015-08-14 10:40:03 +0100

    changed text

--- src/main/java/miky/App.java
+++ src/main/java/miky/App.java
@@ -1,14 +1,14 @@
 package miky;
 
 /**
- * Hello world!
+ * Jenkins/Git/Docker
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Second Line!!!" );
+        System.out.println( "This is a test application that shows how to use Jenkins, Git and Docker" );
+        System.out.println( "to build, deploy and run an application in isolated containers" );
     }
 }
